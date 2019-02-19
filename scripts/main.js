77ee9cfb016568c6bfc391355b239c0e88ca7511
MaryCerasa <cerasame@gmail.com>
2019-02-19 13:34:55 +0000

    x and o working

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,5 @@
 $(() => {
 
-
   // Variables if playing with two//
   const player1 = 'X'
   const player2 = 'O'
@@ -28,17 +27,6 @@ $(() => {
       declareWinner(isPlayerOneTurn ? player1 : player2)
     }
   })
-  //
-  // $reset.on('click', function(event){
-  //   let moves = array.prototype.slice.call($('.square'))
-  //   movesMade.map((m) => {
-  //     m.innerHTML= ''
-  //   })
-  //   $winnerContainer.html('')
-  //   $winnerContainer.css('display','none')
-  //   currentTurn = 1
-  //   movesMade = 0
-  // })
 
   function checkforWinner() {
     const xs = []
@@ -50,43 +38,10 @@ $(() => {
         os.push(index)
       }
     })
-    console.log(xs, os)
-    // if (movesMade > 4) {
-    //   let moves = Array.slice.call($('.square'))
-    //   let results = moves.map(function(square) {
-    //     return square.innerHTML
-    //   })
-    //   let winningCombos = [
-    //     [0,1,2],
-    //     [3,4,5],
-    //     [6,7,8],
-    //     [0,3,6],
-    //     [1,4,7],
-    //     [2,5,8],
-    //     [0,4,8],
-    //     [2,4,6]
-    //   ]
-    // }
   }
-
-  function squaresChecked(indexes) {
-    return indexes.every(
-      function(index) {
-        return square[index].innerHTML === 'X'
-      }
-    }
-  }
-
-  if (squaresChecked([0, 1, 2]) || squaresChecked([3, 4, 5]) || squaresChecked([6, 7, 8]) || squaresChecked([0, 3, 6]) || squaresChecked([1, 4, 7]) || squaresChecked([2, 5, 8]) || squaresChecked([0, 4, 8]) || squaresChecked([2, 4, 6]) {
-    alert("Win")
-  }
-  if (winConditions.some(squaresChecked)) {
-    alert("Win")
-  }
+  console.log(xs, os)
 
 
-  // function declareWinner(winner) {
-  //
-  // }
+
 
 })
